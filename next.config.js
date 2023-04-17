9b831183961cc7fe34e0733df8848ef3e049a70d
FixingPixels <chris@fixingpixels.com>
2023-04-17 10:38:29 -0400

    revert back

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.experiments = {
+        topLevelAwait: true,
+      };
+    }
     return config;
   },
 };
